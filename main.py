e0b57d02442b54c588b68ee45c02bd5ec983fc23
ELB <aaaa.elb@gmail.com>
2026-07-22 09:49:34 -0300

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open('dados.json') as f:
 intents = discord.Intents.default()
 client = discord.Client(command_prefix = ".", intents=intents, status = discord.Status.idle, activity = discord.Game("ELBot em desenvolvimento"))
 
-guild = discord.Object(id=1313924353926365237)
+enclaves = discord.Object(id=1313924353926365237)
 
 class dependencias(discord.Client):
     def __init__(self):
@@ -16,13 +16,13 @@ class dependencias(discord.Client):
         self.tree = app_commands.CommandTree(self)
 
     async def setup_hook(self):
-        await self.tree.sync(guild=guild) # None = global / guild = server específico
+        await self.tree.sync(guild=enclaves) # None = global / guild = server específico
         print('\n -- Comandos globais sincronizados\n')
         for cmd in self.tree.get_commands():
             print(f'Comando: {cmd.name} - {cmd.description}')
         print('\n')
         print('\n -- Comandos do servidor sincronizados\n')
-        for cmd in self.tree.get_commands(guild=guild):
+        for cmd in self.tree.get_commands(guild=enclaves):
             print(f'Comando: {cmd.name} - {cmd.description}')
         print('\n')
 
@@ -52,22 +52,22 @@ async def on_typing(channel, user, when): #comando para quando alguem está digi
 '''
 
 # comandos de barra daqui pra baixo
-@client.tree.command(name="ping", description="Responde com pong!", guild=guild)
+@client.tree.command(name="ping", description="Responde com pong!", guild=enclaves)
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("Pong!")
     return
 
-@client.tree.command(name="abrido", description="Comando secreto dos enclavos", guild=guild)
+@client.tree.command(name="abrido", description="Comando secreto dos enclavos", guild=enclaves)
 async def abrido(interaction: discord.Interaction):
     await interaction.response.send_message("Idoso")
     return
 
-@client.tree.command(name="teste", description="afbjkbjk", guild=guild) #único comando atualmente configurado para funcionar em servidor específico.
+@client.tree.command(name="teste", description="afbjkbjk", guild=enclaves) #único comando atualmente configurado para funcionar em servidor específico.
 async def teste(interaction: discord.Interaction):
     await interaction.response.send_message(f'{interaction.user.mention} teste')
     return
 
-@client.tree.command(name="join", description="join em call", guild=guild)
+@client.tree.command(name="join", description="join em call", guild=enclaves)
 async def join(interaction: discord.Interaction):
     if interaction.user.voice is None: #se o usuário não estiver em um canal
         await interaction.response.send_message("Você não está em um canal de voz.")
@@ -81,7 +81,7 @@ async def join(interaction: discord.Interaction):
     await interaction.user.voice.channel.connect()
     return
 
-@client.tree.command(name="leave", description="leave da call", guild=guild)
+@client.tree.command(name="leave", description="leave da call", guild=enclaves)
 async def leave(interaction: discord.Interaction):
     if interaction.guild.voice_client is None: #se o bot não estiver em um canal
         await interaction.response.send_message("Eu não estou conectado a um canal de voz.")
@@ -91,7 +91,7 @@ async def leave(interaction: discord.Interaction):
     await interaction.guild.voice_client.disconnect()
     return
 
-@client.tree.command(name="play", description="toca um arquivo de áudio", guild=guild)
+@client.tree.command(name="play", description="toca um arquivo de áudio", guild=enclaves)
 async def play(interaction: discord.Interaction):
     if interaction.guild.voice_client is None: #se o bot não estiver em um canal
         await interaction.response.send_message("Eu não estou conectado a um canal de voz.")
